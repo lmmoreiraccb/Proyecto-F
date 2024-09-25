62538a5f9e7c3c5489229869728f5e0f21d75544
Erwin José Tomás Camey <ej.tomas@ccb.edu.gt>
2024-09-25 09:21:59 -0600

    contacto1

--- tienda-douglass/src/app/contact/page.js
+++ tienda-douglass/src/app/contact/page.js
@@ -1,10 +1,13 @@
-import Image from "next/image";
-
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-  <div >
-    CONTACTO
-  </div>
+    <main className={styles.imagenes}>
+       <img
+          src="bosquesito.jpg"
+          alt="Imagen"
+          className={styles.imagenes}
+        />
+    </main>
   );
 }
