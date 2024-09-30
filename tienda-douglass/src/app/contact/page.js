e9612040c8892dfdb23e878fd67998c163326471
Erwin José Tomás Camey <ej.tomas@ccb.edu.gt>
2024-09-30 11:19:53 -0600

    buenas

--- tienda-douglass/src/app/contact/page.js
+++ tienda-douglass/src/app/contact/page.js
@@ -1,16 +1,40 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
       <header className={styles.header}>
-        <div >
-          <span >
-            Bienvenidos a
-            <span className={styles.yellowText}></span>
-          </span>
+        <div>
+          <span>
+            Contacto
+            <section className={styles.body}>
+            <div className={styles.infoContainer}>
+          <span className={styles.title + "" + styles.yellowText}></span>
           <br />
-          <span>Tienda Douglass</span>
+          <span className={styles.title}></span>
+          <div className={styles.list}>
+            <ul>
+              <li>
+                <span className={styles.grayText}>Teléfono: </span> 1609-1122
+              </li>
+              <li>
+                <span className={styles.grayText}>Correo: </span> tienda_douglass@gmail.com
+              </li>
+              <li>
+                <span className={styles.grayText}>Dirección: </span> 5ta avenida, 16 calle, Zona 2
+              </li>
+              <li>
+                <span className={styles.grayText}>Instagram: </span> @tienda_douglass_OFICIAL
+              </li>
+              <li>
+                <span className={styles.grayText}>X: </span> @tienda_douglass_OFICIAL
+              </li>
+            </ul>
+          </div>
+          </div>
+            </section>
+          </span>
         </div>
       </header>
     </main>
