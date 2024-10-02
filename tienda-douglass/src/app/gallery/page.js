dede80e0157603b607fea1402a585cc21112a917
Erwin José Tomás Camey <ej.tomas@ccb.edu.gt>
2024-10-02 09:15:10 -0600

    final3

--- tienda-douglass/src/app/gallery/page.js
+++ tienda-douglass/src/app/gallery/page.js
@@ -2,28 +2,100 @@ import styles from "./page.module.css";
 import Navbar from "../components/navbar";
 
 export default function Home() {
-    return (
-      <main>
-        <Navbar></Navbar>
-        <header id="header" className={styles.header}>
-        </header>
-        <section id="Imagenes" className={styles.imagenes}>
-          <img
-            src=".jpg"
-            alt="Imagen"
-            className={styles.image}
-          />
-          <img
-            src=".jpeg"
-            alt="Imagen"
-            className={styles.image}
-          />
-          <img
-            src=".jpg"
-            alt="Imagen"
-            className={styles.image}
-          />
-        </section>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <Navbar></Navbar>
+      <header className={styles.header}>
+        <div flexDirection="column">
+          <section className={styles.imagenes}>
+
+            <img
+              src="amarillo.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+            <img
+              src="brie.jpeg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+            <img
+              src="cheddar.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+          </section>  
+          <section id="Imagenes" className={styles.imagenes}>
+
+            <img
+              src="gouda.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+
+            <img
+              src="mozzarella.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+            <img
+              src="suizo.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+          </section>
+
+          <section className={styles.imagenes}>
+
+            <img
+              src="yogurt.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+
+            <img
+              src="leche.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+            <img
+              src="crema.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+          </section>
+          <section id="Imagenes" className={styles.imagenes}>
+
+            <img
+              src="azul.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+
+            <img
+              src="pita.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+            <img
+              src="quesocrema.jpg"
+              alt="Imagen"
+              className={styles.image}
+            />
+
+          </section>
+
+        </div>
+      </header>
+    </main>
+  );
+}
